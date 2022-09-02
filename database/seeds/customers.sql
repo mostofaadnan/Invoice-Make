59767657074054bfbc8e17a324b078ee66f5c62a
Mostofa Shahadat Adnan <msa.cse.metro@gmail.com>
2022-09-02 06:11:13 +0600

    Delete database directory

--- database/seeds/customers.sql
+++
@@ -1,4 +0,0 @@
-
-INSERT INTO `customers` (`id`, `customerid`, `name`, `address`, `country_id`, `state_id`, `city_id`, `postalcode`, `TIN`, `status`, `mobile_no`, `tell_no`, `fax_no`, `email`, `website`, `image`, `openingDate`, `user_id`, `created_at`, `updated_at`, `description`) VALUES
-(1, '10001', 'Default Customer', 'House:45, Main Street, Alta Loma', 231, 3924, 42809, 7800, NULL, 1, '4433567788', '0821459665', '126568', NULL, 'www.invmake.com', '1602616487JPG', '10/14/2020', 1, '2020-10-13 13:14:48', '2020-10-13 13:14:48', NULL);
-
